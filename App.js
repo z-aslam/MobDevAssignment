8b43311637cf018db3d035345499560eae6f563f
Zainab Aslam <zainabaslam@Zainabs-MacBook-Pro.local>
2023-02-20 21:48:48 +0000

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Text, TextInput, View, Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email:'',
+      password:'',
+      errorText: '',
+    }
+
+  }
+
+  
+
+
+  login = () => {
+    Alert.alert('Email: ' + this.state.email, 'Password: ' + this.state.password);
+
+
+
+    //Check email and password validity
+    if(!this.checkEmail(this.state.email)){
+      this.setState({errorText:'Email in wrong format'});
+      return;
+    }
+    if(!this.checkPassword(this.state.password)){
+      this.setState({errorText:'Password in wrong format'});
+      return;
+    }
+    this.setState({errorText: 'No Errors for now!'});
+  }
+
+  checkEmail = (email) => {
+    //Email check logic
+    return true;
+  }
+
+  checkPassword = (password) => {
+    //Password check logic
+    return false;
+  }
+
+  render() {
+    return (
+      <View style={{
+        flex:1,
+        justifyContent: 'center'
+      }}>
+        <TextInput onChangeText={(text)=>{this.setState({email:text})}} placeholder="Email..."/>
+        <TextInput onChangeText={(text)=>{this.setState({password:text})}} secureTextEntry={true} placeholder="Password..."/>
+        <Button title='Login' onPress={this.login}/>
+
+        <Text>{this.state.errorText}</Text>
+
+      </View>
+      
+    );
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
